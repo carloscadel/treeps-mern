650be1c8f355250747f7c194ed061e451dfd65a4
carloscadel <c.cazallas.delfa@gmail.com>
2019-01-31 18:38:18 +0100

    first commit

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+    }
+  }
+  render() {                
+    return (
+      <div className="Home">
+        <div className="header-div">
+          <div className="header-prof-pic-div">
+            <img className="header-prof-pic" src="Carlos.jpeg" alt="Profile" />
+          </div>
+          <div className="header-prof-text-div">
+            <div className="header-prof-text-name-div">
+              <h3>Carlos, 35</h3>
+            </div>
+            <div className="header-prof-text-status-div">
+              <h4>Planning my trip to Myanmar and Thailand for February!!</h4>
+            </div>
+          </div>
+        </div>
+        <div className="separator-div">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
